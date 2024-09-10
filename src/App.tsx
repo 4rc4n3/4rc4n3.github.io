efe841740bf9ff2866d5647b9fb383cd7991c480
Nikola Pavlovic <npavlovic101@gmail.com>
2024-09-10 11:36:37 +0200

    fix syntax

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,3 @@
-export const App() {
+export const App = () => {
   return <h1><img src="https://avatars.githubusercontent.com/in/751136?s=30&u=b1d8f18898c72eb6058ca5c69dc0c57323d9eaab&v=4" />C0N9U17 by 4rc4n3</h1>
 }
