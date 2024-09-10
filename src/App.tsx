4e6bb6f8485f89407c5e2fdedfc6e00f4dca4d06
Nikola Pavlovic <npavlovic101@gmail.com>
2024-09-10 19:43:08 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,80 @@
-export const App = () => {
-  return <h1><img src="https://avatars.githubusercontent.com/in/751136?s=30&u=b1d8f18898c72eb6058ca5c69dc0c57323d9eaab&v=4" />C0N9U17 by 4rc4n3</h1>
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { createTheme } from '@mui/material/styles';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { AppProvider } from '@toolpad/core/AppProvider';
+import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import type { Navigation, Router } from '@toolpad/core';
+
+const NAVIGATION: Navigation = [
+  {
+    segment: 'dashboard',
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: 'orders',
+    title: 'Orders',
+    icon: <ShoppingCartIcon />,
+  },
+];
+
+const theme = createTheme({
+  colorSchemes: { dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function DemoPageContent({ pathname }: { pathname: string }) {
+  return (
+    <Box
+      sx={{
+        py: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        textAlign: 'center',
+      }}
+    >
+      <Typography>Dashboard content for {pathname}</Typography>
+    </Box>
+  );
+}
+
+export const App() {
+  const [pathname, setPathname] = React.useState('/dashboard');
+
+  const router = React.useMemo<Router>(() => {
+    return {
+      pathname,
+      searchParams: new URLSearchParams(),
+      navigate: (path) => setPathname(String(path)),
+    };
+  }, [pathname]);
+
+  return (
+    <AppProvider
+      navigation={NAVIGATION}
+      branding={{
+        logo: <img src="https://avatars.githubusercontent.com/in/751136?s=30&u=b1d8f18898c72eb6058ca5c69dc0c57323d9eaab&v=4" />,
+        title: 'C0N9U17',
+        subtitle: '4rc4n3'
+      }}
+      router={router}
+      theme={theme}
+    >
+      <DashboardLayout>
+        <DemoPageContent pathname={pathname} />
+      </DashboardLayout>
+    </AppProvider>
+  );
 }
